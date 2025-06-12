36b47ff9f9e4a118ce668c9c6b180f22f61bb44a
Shadow105EMM <elijasmmai@gmx.ch>
2025-06-12 21:47:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -12,10 +12,52 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix =".", intents=intents)
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+Modrole = "Moderator"
 
 @bot.event
 async def on_ready():
-    print(f"Welcome {bot.user.name}")
+    print(f"We are ready to go in, {bot.user.name}")
+
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server {member.name}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    if "shit" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Don't use that word!")
+    
+    if "asshole" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Don't use that word!")
+
+    if "fuck" in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} - Don't use that word!")
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def hey(ctx):
+    await ctx.send(f"Hello {ctx.author.mention}!")
+
+@bot.command()
+async def assign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=Modrole)
+    if role:
+        await ctx.author.add_roles(role)
+        await ctx.send(f"{ctx.author.mention} is now assigned to {Modrole}.")
+    else: 
+        await ctx.send("Role doesn't exist.")
+
+@bot.command()
+async def dosomething(ctx):
+    await ctx.send("I did something")
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
